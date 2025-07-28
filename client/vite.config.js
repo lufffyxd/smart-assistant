6798ef8bcdc739e5d9be9da5627da30839a210e7
lufffyxd <otmanine.dev@gmail.com>
2025-07-28 12:33:32 +0100

    Fix Vite config: move index.html to root and remove custom input

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/  
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
   },
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html'
-    }
+    outDir: 'dist'
+    // Remove the rollupOptions.input - let Vite use default
   }
 })
